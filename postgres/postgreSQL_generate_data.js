582c0ef88a9aa022fff29e522e6e5a4065f9e7e7
Nuh <numoh.55@gmail.com>
2024-05-17 10:45:03 +0200

    A little fix to performQuery in PostgreSQL

--- postgres/postgreSQL_generate_data.js
+++ postgres/postgreSQL_generate_data.js
@@ -188,7 +188,7 @@ function run_pgbench(command) {
 
 async function performQueryPostgresForMonth() {
     try {
-        const command = `docker exec postgres_container pgbench -U numoh -d postgres_for_test -c "SELECT EXTRACT(EPOCH FROM timezone('Europe/Berlin', date_trunc('month', timezone('Europe/Berlin', m.timestamp))))::integer AS ts, SUM(value) AS value, d.sub_type AS type FROM measurements AS m JOIN devices AS d ON d.id = m.device_id JOIN organisations AS o ON d.id = o.device_id WHERE o.organisation_id = '1' AND m.type = 5 AND m.timestamp >= TO_TIMESTAMP(1704106800) AND m.timestamp < TO_TIMESTAMP(1706698800) GROUP BY date_trunc('month', timezone('Europe/Berlin', m.timestamp)), d.sub_type ORDER BY date_trunc('month', timezone('Europe/Berlin', m.timestamp)), d.sub_type;" --transactions=10 --log`;
+        const command = `docker exec postgres_container bash -c \ "QUERY=\"SELECT EXTRACT(EPOCH FROM timezone('Europe/Berlin', date_trunc('month', timezone('Europe/Berlin', m.timestamp))))::integer AS ts, SUM(value) AS value, d.sub_type AS type FROM measurements AS m JOIN devices AS d ON d.id = m.device_id JOIN organisations AS o ON d.id = o.device_id WHERE o.organisation_id = '1' AND m.type = 5 AND m.timestamp >= TO_TIMESTAMP(1704106800) AND m.timestamp < TO_TIMESTAMP(1706698800) GROUP BY date_trunc('month', timezone('Europe/Berlin', m.timestamp)), d.sub_type ORDER BY date_trunc('month', timezone('Europe/Berlin', m.timestamp)), d.sub_type;\" && \ pgbench -U numoh -d postgres_for_test -f <(echo \"\$QUERY\") --transactions=10 --log"`;
         const result = await run_pgbench(command);
 
         console.log("Benchmarked 10 queries successfully!");
@@ -201,7 +201,7 @@ async function performQueryPostgresForMonth() {
 
 async function performQueryPostgresForYear() {
     try {
-        const command = `docker exec postgres_container pgbench -U numoh -d postgres_for_test -c "SELECT EXTRACT(EPOCH FROM timezone('Europe/Berlin', date_trunc('year', timezone('Europe/Berlin', m.timestamp))))::integer AS ts, SUM(value) AS value, d.sub_type AS type FROM measurements AS m JOIN devices AS d ON d.id = m.device_id JOIN organisations AS o ON d.id = o.device_id WHERE o.organisation_id = '1' AND m.type = 5 AND m.timestamp >= TO_TIMESTAMP(1704106800) AND m.timestamp < TO_TIMESTAMP(1735642800) GROUP BY date_trunc('year', timezone('Europe/Berlin', m.timestamp)), d.sub_type ORDER BY date_trunc('year', timezone('Europe/Berlin', m.timestamp)), d.sub_type;" --transactions=10 --log`;
+        const command = `docker exec postgres_container bash -c \ "QUERY=\"SELECT EXTRACT(EPOCH FROM timezone('Europe/Berlin', date_trunc('year', timezone('Europe/Berlin', m.timestamp))))::integer AS ts, SUM(value) AS value, d.sub_type AS type FROM measurements AS m JOIN devices AS d ON d.id = m.device_id JOIN organisations AS o ON d.id = o.device_id WHERE o.organisation_id = '1' AND m.type = 5 AND m.timestamp >= TO_TIMESTAMP(1704106800) AND m.timestamp < TO_TIMESTAMP(1735642800) GROUP BY date_trunc('year', timezone('Europe/Berlin', m.timestamp)), d.sub_type ORDER BY date_trunc('year', timezone('Europe/Berlin', m.timestamp)), d.sub_type;\" && \ pgbench -U numoh -d postgres_for_test -f <(echo \"\$QUERY\") --transactions=10 --log"`;
         const result = await run_pgbench(command);
 
         console.log("Benchmarked 10 queries successfully!");
@@ -288,7 +288,7 @@ async function extractExecutionTime(file) {
 
             const cv = (standardDeviation / mean) * 100;
 
-            console.log("Coefficient of Variation:", cv.toFixed(2) + "%");
+            console.log("Coefficient of Variation:", cv.toFixed(2) + " %");
 
             minMeanTime = mean - standardDeviation;
             maxMeanTime = mean + standardDeviation;
